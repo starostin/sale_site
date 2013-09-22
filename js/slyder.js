cb66b0fe3c95d398cdf7627deff52fbb89e8920e
starostin <starostinoleg89@mail.ru>
2013-09-22 18:38:06 +0300

    Search results page

--- js/slyder.js
+++ js/slyder.js
@@ -6,34 +6,36 @@
  * To change this template use File | Settings | File Templates.
  */
 $(document).ready(function(){
-    var arrow_left = $('#slyder').find('li').eq(0);
-    var arrow_right = $('#slyder').find('li').eq(6);
-    var un_arrow_left = $('#un_slyder').find('li').eq(0);
-    var un_arrow_right = $('#un_slyder').find('li').eq(6);
+    var goods = $('.goods'),
+        university = $('.un_slyder'),
+        arrow_left = goods.find('li:first-child'),
+        arrow_right = goods.find('li:last-child'),
+        un_arrow_left = university.find('li:first-child'),
+        un_arrow_right = university.find('li:last-child');
     arrow_left.on('click',function(){
-        var last = $('#slyder').find('li').eq(5);
-        var first = $('#slyder').find('li').eq(1);
+        var last = goods.find(arrow_right).prev(),
+            first = goods.find(arrow_left).next();
         first.hide();
         $(last).after(first);
         first.show();
     });
     arrow_right.on('click', function(){
-        var last = $('#slyder').find('li').eq(5);
-        var first = $('#slyder').find('li').eq(1);
+        var last = goods.find(arrow_right).prev(),
+            first = goods.find(arrow_left).next();
         last.hide();
         $(first).before(last);
         last.show();
-    })
+    });
     un_arrow_left.on('click',function(){
-        var last = $('#un_slyder').find('li').eq(5);
-        var first = $('#un_slyder').find('li').eq(1);
+        var last = university.find(un_arrow_right).prev(),
+            first = university.find(un_arrow_left).next();
         first.hide();
         $(last).after(first);
         first.show();
     });
     un_arrow_right.on('click', function(){
-        var last = $('#un_slyder').find('li').eq(5);
-        var first = $('#un_slyder').find('li').eq(1);
+        var last = university.find(un_arrow_right).prev(),
+            first = university.find(un_arrow_left).next();
         last.hide();
         $(first).before(last);
         last.show();
